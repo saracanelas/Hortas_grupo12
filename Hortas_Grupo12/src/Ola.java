1ce748dfdb76e2424a6be62d781ddbea4eb0d148
chiicaa15 <joaomarques0243@gmail.com>
2025-09-17 20:47:47 +0100

    criação de gets e sets

--- Hortas_Grupo12/src/Ola.java
+++ Hortas_Grupo12/src/Ola.java
@@ -7,5 +7,33 @@ public class Ola {
 		this.id = id;
 		this.tipo = tipo;
 	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the tipo
+	 */
+	public String getTipo() {
+		return tipo;
+	}
+
+	/**
+	 * @param tipo the tipo to set
+	 */
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
 	
 }

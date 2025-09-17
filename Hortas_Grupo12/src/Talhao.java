3cc78b6b679ef32c0b6795dcffe58e6fd060ee07
Beatriz Pimentel <Beatriz Pimentel@Lua.home>
2025-09-17 22:15:09 +0100

    realização dos getters e setters da classe talhao

--- Hortas_Grupo12/src/Talhao.java
+++ Hortas_Grupo12/src/Talhao.java
@@ -8,6 +8,30 @@
 public class Talhao {
 	private int id;
 	private float area;
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the area
+	 */
+	public float getArea() {
+		return area;
+	}
+	/**
+	 * @param area the area to set
+	 */
+	public void setArea(float area) {
+		this.area = area;
+	}
 	
 	
 

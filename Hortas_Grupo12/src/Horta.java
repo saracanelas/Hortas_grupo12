ed8987eaf170b35533f447ca2062d44cc0b33c56
saracanelas <sara.i.canelas@gmail.com>
2025-09-17 19:55:40 +0100

    Primeira versão da classe horta

---
+++ Hortas_Grupo12/src/Horta.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class Horta {
+	
+	private String nome;
+	private String local;
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getLocal() {
+		return local;
+	}
+	
+	
+
+}

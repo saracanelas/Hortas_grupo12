ec2624602ca7ddd87ddd30c5be9bb0acc7ebd138
51460 <51460@pc40809.labs.uportu.pt>
2025-09-18 09:36:58 +0100

    criação da classe gerir e do construtor

---
+++ Hortas_Grupo12/src/GerirHorta.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+import java.util.ArrayList;
+
+/**
+ * 
+ */
+public class GerirHorta {
+	private ArrayList<Horta>hortas;
+
+	public GerirHorta(ArrayList<Horta> hortas) {
+		super();
+		this.hortas = hortas;
+	}
+	
+	
+
+}
